8210508a23c26e8658d9f4d8f50bb33d3fa72a1c
shivesh9922-blip <shivesh9922@gmail.com>
2025-08-03 18:28:49 +0530

    Create 09_PatternProgram.java

---
+++ Step 01 : Learn the basics/Lec 2: Build-up Logical Thinking/09_PatternProgram.java
@@ -0,0 +1,41 @@
+/*
+////////
+    *
+   ***
+  *****
+ *******
+*********
+*********
+ *******
+  *****
+   ***
+    *
+/////////
+*/
+class Solution {
+        public static void nStarDiamond(int n) {
+        // upper part
+        for (int i = 1; i <= n; i++) {
+            for (int s = 1; s <= n - i; s++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= 2 * i - 1; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        // lower part
+        for (int i = n; i >= 1; i--) {
+            for (int s = 1; s <= n - i; s++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= 2 * i - 1; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+   public static void main(String[] args) {
+       nStarDiamond(5);
+    }
+}
