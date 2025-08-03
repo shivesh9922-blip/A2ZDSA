40d3355fa84969774d9983d11519183b6507f251
shivesh9922-blip <shivesh9922@gmail.com>
2025-08-03 18:21:59 +0530

    Create 07_PatternProgram.java

---
+++ Step 01 : Learn the basics/Lec 2: Build-up Logical Thinking/07_PatternProgram.java
@@ -0,0 +1,26 @@
+/*
+////////
+   *
+   ***
+  *****
+ *******
+*********
+/////////
+*/
+class Solution {
+        public static void nStarTriangle(int n) {
+        for (int i = 1; i <= n; i++) {
+            for (int s = 1; s <= n - i; s++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= 2 * i - 1; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+        
+   public static void main(String[] args) {
+       nStarTriangle(5);
+    }
+}
