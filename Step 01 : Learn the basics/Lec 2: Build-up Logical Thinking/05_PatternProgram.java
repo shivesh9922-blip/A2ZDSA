8ed866c1f5548a9dc4a63addf1d07524ec121b7d
shivesh9922-blip <shivesh9922@gmail.com>
2025-08-03 06:21:04 +0530

    Create 05_PatternProgram.java

---
+++ Step 01 : Learn the basics/Lec 2: Build-up Logical Thinking/05_PatternProgram.java
@@ -0,0 +1,22 @@
+ /*
+/////////OUTPUT
+* * * * * 
+* * * * 
+* * * 
+* * 
+* 
+/////////
+  */ 
+class Solution {
+    public static void seeding(int n) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n - i + 1; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String[] args) {
+       seeding(5);
+    }
+}
