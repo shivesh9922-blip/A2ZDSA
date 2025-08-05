fa3bfe85c91ef962ae25269c4583faf32cff250b
shivesh9922-blip <shivesh9922@gmail.com>
2025-08-05 22:09:38 +0530

    Create 11_PatternProgram.java

---
+++ Step 01 : Learn the basics/Lec 2: Build-up Logical Thinking/11_PatternProgram.java
@@ -0,0 +1,27 @@
+/*
+
+1 
+0 1 
+1 0 1 
+0 1 0 1 
+1 0 1 0 1 
+
+*/
+public class Solution {
+    public static void nBinaryTriangle(int n) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                if ((i + j) % 2 == 0) {
+                    System.out.print(1 + " ");
+                } else {
+                    System.out.print(0 + " ");
+                }
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String[] args)
+    {
+        nBinaryTriangle(5);
+    }
+}
