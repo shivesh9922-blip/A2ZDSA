294d0d008f88113bdf41bbdde1234e4831877e28
shivesh9922-blip <shivesh9922@gmail.com>
2025-08-06 22:49:46 +0530

    Create 21_PatternProgram.java

---
+++ Step 01 : Learn the basics/Lec 2: Build-up Logical Thinking/21_PatternProgram.java
@@ -0,0 +1,26 @@
+/*
+/////
+*****
+*   *
+*   *
+*   *
+*****
+////
+*/
+class Solution {
+   public static void getStarPattern(int n) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (i == 1 || i == n || j == 1 || j == n) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String[] args) {
+        getStarPattern(5);
+    }
+}
